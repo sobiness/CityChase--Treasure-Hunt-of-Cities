61497aa930e81839ef3ca69a2c46f43046f1f9c9
Sobika Sree Ramesh <63402137+sobiness@users.noreply.github.com>
2023-04-24 13:02:03 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-$url = parse_url(getenv("mysql://b1d65545ce4e8b:ea2f8a3b@us-cdbr-east-06.cleardb.net/heroku_1e9cc41f48fdcdc?reconnect=true"));
+//$url = parse_url(getenv("mysql://b1d65545ce4e8b:ea2f8a3b@us-cdbr-east-06.cleardb.net/heroku_1e9cc41f48fdcdc?reconnect=true"));
 
   $server = "us-cdbr-east-06.cleardb.net";
     $username = "b1d65545ce4e8b";
