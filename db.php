ad3aabc9aed784f152dda30ce302bfe6cbd1fcb0
Sobika Sree Ramesh <63402137+sobiness@users.noreply.github.com>
2023-04-24 11:51:24 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,14 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
     $con = mysqli_connect("us-cdbr-east-06.cleardb.net","b1d65545ce4e8b","ea2f8a3b","LoginSystem");
     // Check connection
     if (mysqli_connect_errno()){
