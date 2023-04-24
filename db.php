4a16e9606932e726c0c5e3e9554dc3083ad693dc
Sobika Sree Ramesh <63402137+sobiness@users.noreply.github.com>
2023-04-24 13:13:06 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,15 @@
-<?php
+
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
 //$url = parse_url(getenv("mysql://b1d65545ce4e8b:ea2f8a3b@us-cdbr-east-06.cleardb.net/heroku_1e9cc41f48fdcdc?reconnect=true"));
 
-  $server = "us-cdbr-east-06.cleardb.net";
-    $username = "b1d65545ce4e8b";
-    $password = "ea2f8a3b";
-    $db = "heroku_1e9cc41f48fdcdc";
-
-$conn = new mysqli($server, $username, $password, $db);
-   // $con = mysqli_connect("us-cdbr-east-06.cleardb.net","b1d65545ce4e8b","ea2f8a3b","LoginSystem");
+  <?php
+    // Enter your host name, database username, password, and database name.
+    // If you have not set database password on localhost then set empty.
+    $con = mysqli_connect("sql12.freemysqlhosting.net","sql12614154","JI7dVk6zvR","sql12614154");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 ?>
+
