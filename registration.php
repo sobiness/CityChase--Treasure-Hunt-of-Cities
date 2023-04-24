5d8dc45c1c24638a69fd925de389277e9c11091f
Sobika Sree Ramesh <63402137+sobiness@users.noreply.github.com>
2023-04-24 06:08:13 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -19,9 +19,9 @@
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
         $create_datetime = date("Y-m-d H:i:s");
-        $high_score = $_POST['high_score'];
-        $query    = "INSERT into `users` (username, password, email, create_datetime, high_score)
-                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime','$high_score')";
+        
+        $query    = "INSERT into `users` (username, password, email, create_datetime)
+                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
         $result   = mysqli_query($con, $query);
         if ($result) {
             echo "<div class='form'>
