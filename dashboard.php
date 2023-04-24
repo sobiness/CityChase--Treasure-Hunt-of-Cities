a445ee64858ffa500bcb83a3de5503a285ba86b6
Sobika Sree Ramesh <63402137+sobiness@users.noreply.github.com>
2023-04-24 07:26:23 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -11,9 +11,21 @@ include("auth_session.php");
 </head>
 <body>
     <div class="form">
-        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
-        <p>You are now user dashboard page.</p>
-        <p><a href="Tresht.html">Click to play</a></p>
+    <h1 class="login-title">CityChase</h1>
+        <p><h2 class="login-title">Hey, <?php echo $_SESSION['username']; ?>!</h2></p>
+        <h3 class="login-title">Here are the Rules:</h3>
+    
+        <h3 class="login-title"><ul li>The game is played on a map with longitude and latitude coordinates.</ul li>
+
+<ul li>The player starts by clicking on any location on the map to guess the treasure location.</ul li>
+<ul li>After the player makes a guess, the game checks if the guess is within a certain distance (e.g., 50kms) of the actual treasure location.</ul li>
+<ul li>If the guess is within the acceptable distance of the treasure location, the player wins the game and the treasure location is revealed with a pop-up message.</ul li>
+<ul li>If the guess is outside the acceptable distance of the treasure location, the player receives a clue indicating whether the guess is too far to the north, south, east, or west of the treasure location.</ul li>
+<ul li>The player continues to make guesses and receive clues until they correctly guess the treasure location or choose to end the game.</ul li>
+<ul li>Once the game is ended, the actual treasure location is revealed to the player, and the game is over.
+The player can choose to start a new game by clicking the restart button.</ul li></h3>
+<p><center><a href="Tresht.html" class="login-button">Click to play</a></center></p>
+
     </div>
 </body>
 </html>
